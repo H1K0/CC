af022c38cf0ca7d567d853d60ae9cb92e81af2b8
Masahiko AMANO <49252650+H1K0@users.noreply.github.com>
2020-04-20 22:54:32 +0300

    Let's get it started!
    Now the H1K0-CC is available!

---
+++ files/scripts/albums/build.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+	var body_content=`<header>${title}</header><input class="back-btn" type="button" onclick="history.back();" `
+	if (lang=='en') body_content+=`value="Back"/>`;
+	else if (lang=='ru') body_content+=`value="Назад"/>`;
+	else if (lang=='jp') body_content+=`value="戻る"/>`;
+	for (let i = 1; i <= len; i++) {
+		body_content+=`<div class="photo"><img src="../../files/images/albums/${link}/${(i<100)?'0':''}${(i<10)?'0':''}${i}.jpg" `;
+		if (lang=='en') body_content+=`alt="${title}: Photo #${i}"></div>`;
+		else if (lang=='ru') body_content+=`alt="${title}: Фото №${i}"></div>`;
+		else if (lang=='jp') body_content+=`alt="${title}: 第${i}写真"></div>`;
+	}
+	document.body.innerHTML=body_content;
+};
