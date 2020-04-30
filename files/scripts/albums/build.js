4f33c4ef041ab3e8b0c1b6120fc998024f297c92
H1K0 <hiko4679@gmail.com>
2020-05-01 01:55:34 +0300

    Added "title" attribute for pics in articles and albums
    Well, speaking about albums, I designed the gallery pages with photos but as long as there're some photos bigger than 25MB I cannot upload them. Maybe sometime somehow...

--- files/scripts/albums/build.js
+++ files/scripts/albums/build.js
@@ -5,9 +5,9 @@ window.onload=function(){
 	else if (lang=='jp') body_content+=`value="戻る"/>`;
 	for (let i = 1; i <= len; i++) {
 		body_content+=`<div class="photo"><img src="../../files/images/albums/${link}/${(i<100)?'0':''}${(i<10)?'0':''}${i}.jpg" `;
-		if (lang=='en') body_content+=`alt="${title}: Photo #${i}"></div>`;
-		else if (lang=='ru') body_content+=`alt="${title}: Фото №${i}"></div>`;
-		else if (lang=='jp') body_content+=`alt="${title}: 第${i}写真"></div>`;
+		if (lang=='en') body_content+=`alt="${title}: Photo #${i}" title="${title}: Photo #${i}"></div>`;
+		else if (lang=='ru') body_content+=`alt="${title}: Фото №${i}" title="${title}: Фото №${i}"></div>`;
+		else if (lang=='jp') body_content+=`alt="${title}: 第${i}写真" title="${title}: 第${i}写真"></div>`;
 	}
 	document.body.innerHTML=body_content;
 };
