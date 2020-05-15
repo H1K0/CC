55a37b4941f40302cc4861559bc2234685e6f385
H1K0 <hiko-19@yandex.com>
2020-05-16 00:39:38 +0300

    Some corrections in albums' names

--- files/scripts/PreviewMaker.py
+++ files/scripts/PreviewMaker.py
@@ -9,17 +9,18 @@ def main(path):
 		if file[-4:]=='.jpg':
 			pic=Image.open(f'{path}/{file}')
 			win,hin=pic.size
-			if win>=hin:
-				wout=1000
-				hout=int(1000/win*hin)
-			else:
-				wout=int(1000/hin*win)
-				hout=1000
-			pic.resize((wout,hout)).save(f'{path}/preview/{file}')
-			print()
-			print(f'File: {path}/{file}')
-			print(f'Input size:  {win}x{hin} px')
-			print(f'Output size: {wout}x{hout} px')
+			if win>1000 and hin>1000:
+				if win>=hin:
+					wout=1000
+					hout=1000*hin//win
+				else:
+					wout=1000*win//hin
+					hout=1000
+				pic.resize((wout,hout)).save(f'{path}/preview/{file}')
+				print()
+				print(f'File: {path}/{file}')
+				print(f'Input size:  {win}x{hin} px')
+				print(f'Output size: {wout}x{hout} px')
 
 print('Directory to make previews (type # to exit):')
 req=input('../images/')
